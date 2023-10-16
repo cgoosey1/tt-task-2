3133323ee1dd582db582e2a7b1a0cc0af863d77f
Chris Goosey <chrisgoosey@Chriss-MacBook-Pro.local>
2023-10-16 14:01:23 +0100

    Filter schools on country in report

--- resources/views/site/schools/report.blade.php
+++ resources/views/site/schools/report.blade.php
@@ -2,23 +2,51 @@
 
 @section('content')
     <h3>Schools Members Report</h3>
+    <div class="mb-3">
+        <label for="countries" class="form-label">Showing members for countries</label>
+        <select class="form-select" multiple name="countries[]" id="countries" required>
+            @foreach ($schools->pluck('country') as $country)
+                <option selected>{{ $country }}</option>
+            @endforeach
+        </select>
+    </div>
     <canvas id="schoolsGraph" width="400" height="100" role="img"></canvas>
 @endsection
 
 @section('javascript')
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
     <script>
-        const data = {
-            labels: @json($schools->pluck('name')),
-            datasets: [{
-                data: @json($schools->pluck('members_count'))
-                // Should probably set a nice set of colours for the system to use here, but keeping it simple
-            }]
-        };
+        const schoolData = @json($schools)
+
+        $('select#countries').click(function() {
+            chart.data = graphData();
+            chart.update();
+        });
+
+        function graphData() {
+            let data = [];
+            let countries = $('select#countries').val();
+            $.each(schoolData, function (index, school) {
+                if (countries.includes(school.country)) {
+                    data.push(school.members_count);
+                }
+                else {
+                    data.push(0);
+                }
+            });
+
+            return {
+                labels: @json($schools->pluck('name')),
+                datasets: [{
+                    data: data
+                    // Should probably set a nice set of colours for the system to use here, but keeping it simple
+                }]
+            };
+        }
 
         const config = {
             type: 'bar',
-            data: data,
+            data: graphData(),
             options: {
                 responsive: true,
                 plugins: {
@@ -30,6 +58,6 @@
         };
 
         const ctx = document.getElementById('schoolsGraph').getContext('2d');
-        new Chart(ctx, config);
+        const chart = new Chart(ctx, config);
     </script>
 @endsection

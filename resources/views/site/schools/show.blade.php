38c25eff0d291b814af5639015e9dca9b4af2868
Chris Goosey <chrisgoosey@Chriss-MacBook-Pro.local>
2023-10-16 12:00:24 +0100

    Add schools members list view

--- resources/views/site/schools/show.blade.php
+++ resources/views/site/schools/show.blade.php
@@ -1,5 +1,21 @@
 @extends('layout.master')
 
 @section('content')
-
+    <h3>Members attending {{ $school->name }}</h3>
+    <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($members as $member)
+                <tr>
+                    <td>{{ $member->name }}</td>
+                    <td>{{ $member->email }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
